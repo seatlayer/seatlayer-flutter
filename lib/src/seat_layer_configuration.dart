0849eaed5b5cfd0caebcfa6ba7b8b7e21d13c8ac
navin10sharma <3096611+navin10sharma@users.noreply.github.com>
2026-09-25 09:24:50 +0530

    Report SDK version 0.11.0

--- lib/src/seat_layer_configuration.dart
+++ lib/src/seat_layer_configuration.dart
@@ -4,7 +4,7 @@ import 'open_enums.dart';
 import 'payloads.dart';
 
 /// This SDK's version.
-const String seatLayerSdkVersion = '0.10.0';
+const String seatLayerSdkVersion = '0.11.0';
 
 /// The SeatLayer renderer version this SDK release is pinned to.
 const String seatLayerHostedWebVersion = '0.100.0';
